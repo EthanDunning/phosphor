38d5a01197fcc0756d995a183870e5c64f70c254
EthanDunning <ethandunning2002@gmail.com>
2026-03-07 11:09:58 -0600

    updated version

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -117,7 +117,7 @@ class App extends Component<any, AppState> {
         return (
             <>
                 <header className="phosphor-header">
-                    <span className="phosphor-header__title">PHOSPHOR v1.4</span>
+                    <span className="phosphor-header__title">PHOSPHOR v1.4.1</span>
 
                     <div className="phosphor-header__controls">
                         {uploadError && (
